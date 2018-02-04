981b6a55531793881093c3c210bffffb319824c3
alanaoufal <alanaoufal@gmail.com>
2018-02-04 22:08:29 +0100

    fix gsolve to 100% :)

--- Reinhard/gsolve.py
+++ Reinhard/gsolve.py
@@ -2,6 +2,7 @@ import numpy as N
 import numpy.linalg as lin
 
 def gsolve(Z,B,l,w):
+
     n = 256;
     A = N.zeros((Z.shape[0]*Z.shape[1]+n+1,n+Z.shape[0]));
     b = N.zeros((A.shape[0], 1));
@@ -18,18 +19,17 @@ def gsolve(Z,B,l,w):
 
     # Fix the curve by setting its middle value to 0
     A[k, 128] = 1;
-
     k = k + 1;
 
     # Include the smoothness equations
-    for i in range(0, n-3):
+    for i in range(0, n-2):
         A[k, i] = l * w[i + 1];
         A[k, i + 1] = -2 * l * w[i + 1];
-        A[k, i + 2]=l * w[i + 1];
+        A[k, i + 2] = l * w[i + 1];
         k = k + 1;
     
     # Solve the system using SVD
     x = lin.lstsq(A,b)[0]
-    g = x[1:n];
-    lE = x[n+1:x.shape[0]];
+    g = x[0:n];
+    lE = x[n:x.shape[0]];
     return [g, lE];
--- Reinhard/sample.py
+++ Reinhard/sample.py
@@ -10,18 +10,25 @@ def sample(image, sample_indices, num_pixels):
     green_channel = image[:, :, 1];
     blue_channel = image[:, :, 0];
 
-    red_channel = N.reshape(red_channel,(num_pixels,1));
-    green_channel = N.reshape(green_channel, (num_pixels, 1));
-    blue_channel = N.reshape(blue_channel, (num_pixels, 1));
+    reddish = [];
+    greenish = [];
+    blueish = [];
+    for i in range(0, image.shape[1]):
+        for j in range(0, image.shape[0]):
+            reddish.append(red_channel[j, i]);
+            greenish.append(green_channel[j, i]);
+            blueish.append(blue_channel[j, i]);
+
+    red_channel = N.reshape(reddish,(num_pixels,1));
+    green_channel = N.reshape(greenish, (num_pixels, 1));
+    blue_channel = N.reshape(blueish, (num_pixels, 1));
 
     #red_channel = red_channel.flatten();
     #blue_channel = blue_channel.flatten();
     #green_channel = green_channel.flatten();
 
-
     red = red_channel[(sample_indices,1-1)];
-    green = green_channel[(sample_indices, 1 - 1)];
+    green = green_channel[(sample_indices, 1-1)];
     blue = blue_channel[(sample_indices,1-1)];
-
 
     return [red, green, blue]
